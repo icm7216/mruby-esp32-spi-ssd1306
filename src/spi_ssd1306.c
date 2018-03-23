352e8317048e399a49b5662acc3ddc98ca17df39
icm7216 <icm7216@users.noreply.github.com>
2018-03-23 11:04:20 +0900

    Improve frame buffer

--- src/spi_ssd1306.c
+++ src/spi_ssd1306.c
@@ -22,7 +22,7 @@
 // SSD1306 display config
 #define SSD1306_DISPLAY_WIDTH   128
 #define SSD1306_DISPLAY_HEIGHT  64
-#define SSD1306_DISPLAY_PIXSEL  1024
+#define SSD1306_DISPLAY_PIXEL   1024
 #define SSD1306_FONT_WIDTH      8
 #define SSD1306_FONT_HEIGHT     8 
 
@@ -65,149 +65,159 @@ typedef struct spi_config_t {
   uint32_t spi_freq;        // SPI clock frequency [Hz]
   uint8_t spi_mode;         // SPI mode (0-3)
   uint8_t dma_ch;           // No DMA or DMA channel (1 or 2)
+  bool require_reset;       // Reset the display
   spi_device_handle_t spi;  // Handle for a device on a SPI bus
+  tinygrafx_t tinygrafx;    // Tiny graphics config and frame buffer
 } spi_config_t;
 
 static const char *TAG = "SPI_SSD1306";
 
 
-// ----- Common graphics methods -----
+// ----- Common graphics methods ----------
 // mruby binding of manipulate the graphics
-
+// ----------------------------------------
 static mrb_value
-ssd1306_clear(mrb_state *mrb, mrb_value self)
+lcd_clear(mrb_state *mrb, mrb_value self)
 {
-  buffer_clear();
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
+
+  buffer_clear(tg->tinygrafx);
   return self;
 }
 
 static mrb_value
-ssd1306_set_pixel(mrb_state *mrb, mrb_value self)
+lcd_set_pixel(mrb_state *mrb, mrb_value self)
 {
-	mrb_int x, y;  
+	mrb_int x, y;
   int16_t color;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
   mrb_get_args(mrb, "ii", &x, &y);
-
-	set_pixel(x, y, color); 
+	
+  set_pixel(tg->tinygrafx, x, y, color);
   return mrb_nil_value();
 }
 
 static mrb_value
-ssd1306_get_pixel(mrb_state *mrb, mrb_value self)
+lcd_get_pixel(mrb_state *mrb, mrb_value self)
 {
-	mrb_int x, y;  
+	mrb_int x, y;
   int16_t pixel;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   mrb_get_args(mrb, "ii", &x, &y);
-	pixel = get_pixel(x, y);
-
+	
+  pixel = get_pixel(tg->tinygrafx, x, y);
   return mrb_fixnum_value(pixel);
 }
 
 static mrb_value
-ssd1306_draw_line(mrb_state *mrb, mrb_value self)
+lcd_draw_line(mrb_state *mrb, mrb_value self)
 {
   mrb_int x0, y0, x1, y1;
-  
   int16_t color;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
-  
   mrb_get_args(mrb, "iiii", &x0, &y0, &x1, &y1);
-
   if ((color < BLACK) || (color > INVERT)) {
     color = WHITE;
   }
-  draw_line(x0, y0, x1, y1, color);
   
+  draw_line(tg->tinygrafx, x0, y0, x1, y1, color);
   return mrb_nil_value();
 }
 
 static mrb_value
-ssd1306_draw_vertical_line(mrb_state *mrb, mrb_value self)
+lcd_draw_vertical_line(mrb_state *mrb, mrb_value self)
 {
 	mrb_int x, y, h;
   int16_t color;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
   mrb_get_args(mrb, "iii", &x, &y, &h);
-
-	draw_vertical_line(x, y, h, color);
-	return mrb_nil_value();
+	
+  draw_vertical_line(tg->tinygrafx, x, y, h, color);
+  return mrb_nil_value();
 }
 
 static mrb_value
-ssd1306_draw_horizontal_line(mrb_state *mrb, mrb_value self)
+lcd_draw_horizontal_line(mrb_state *mrb, mrb_value self)
 {
 	mrb_int x, y, w;
   int16_t color;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
   mrb_get_args(mrb, "iii", &x, &y, &w);
-
-	draw_horizontal_line(x, y, w, color);
+	
+  draw_horizontal_line(tg->tinygrafx, x, y, w, color);
 	return mrb_nil_value();
 }
 
 static mrb_value
-ssd1306_draw_rect(mrb_state *mrb, mrb_value self)
+lcd_draw_rect(mrb_state *mrb, mrb_value self)
 {
 	mrb_int x, y, w, h;
   int16_t color;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
   mrb_get_args(mrb, "iiii", &x, &y, &w, &h);
-
-	draw_rect(x, y, w, h, color);
+	
+  draw_rect(tg->tinygrafx, x, y, w, h, color);
 	return mrb_nil_value();
 }
 
 static mrb_value
-ssd1306_draw_fill_rect(mrb_state *mrb, mrb_value self)
+lcd_draw_fill_rect(mrb_state *mrb, mrb_value self)
 {
 	mrb_int x, y, w, h;
   int16_t color;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
   mrb_get_args(mrb, "iiii", &x, &y, &w, &h);
-
-	draw_fill_rect(x, y, w, h, color);
+	
+  draw_fill_rect(tg->tinygrafx, x, y, w, h, color);
 	return mrb_nil_value();
 }
 
 static mrb_value
-ssd1306_draw_circle(mrb_state *mrb, mrb_value self)
+lcd_draw_circle(mrb_state *mrb, mrb_value self)
 {
 	mrb_int x, y, r;
   int16_t color;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
   mrb_get_args(mrb, "iii", &x, &y, &r);
-
-	draw_circle(x, y, r, color);
+	
+  draw_circle(tg->tinygrafx, x, y, r, color);
 	return mrb_nil_value();
 }
 
 static mrb_value
-ssd1306_draw_fill_circle(mrb_state *mrb, mrb_value self)
+lcd_draw_fill_circle(mrb_state *mrb, mrb_value self)
 {
   mrb_int x, y, r;
   int16_t color;
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
   mrb_get_args(mrb, "iii", &x, &y, &r);
-
-	draw_fill_circle(x, y, r, color);
+	
+  draw_fill_circle(tg->tinygrafx, x, y, r, color);
 	return mrb_nil_value();
 }
 
 // mruby binding of Display a character string
 static mrb_value
-ssd1306_text(mrb_state *mrb, mrb_value self)
+lcd_text(mrb_state *mrb, mrb_value self)
 {
   mrb_int x, y;
   mrb_value data;
   int16_t color, fontsize;
-
+  spi_config_t *tg = (spi_config_t *)DATA_PTR(self);
   color = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@color")));
   fontsize = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@fontsize")));
   mrb_get_args(mrb, "iiS", &x, &y, &data);
-  display_text(x, y, RSTRING_PTR(data), RSTRING_LEN(data), color, fontsize);
+  
+  display_text(tg->tinygrafx, x, y, RSTRING_PTR(data), RSTRING_LEN(data), color, fontsize);
   // ESP_LOGI(TAG, "color:%d, size:%d, text:%s", color, fontsize, RSTRING_PTR(data));
-
   return mrb_nil_value();
 }
 // ----- Common graphics methods -----
@@ -217,29 +227,18 @@ ssd1306_text(mrb_state *mrb, mrb_value self)
 
 // ----- SSD1306 methods and functions -----
 
-// This function is called (in irq context!) just before a transmission starts.
-// It will set the D/C line to the value indicated in the user field.
-static void
-spi_pre_transfer_callback(spi_transaction_t *t)
-{
-  uint32_t dc = (uint32_t)t->user;
-  gpio_set_level(SSD1306SPI_PIN_NUM_DC, dc);
-}
 
-// reset the D/C line
-static void
-spi_post_transfer_callback(spi_transaction_t *t)
-{
-  gpio_set_level(SSD1306SPI_PIN_NUM_DC, 0);
-}
-
-// Send buffer data to the OLED
+// Send buffer data to the display
 // NOTE: NO_DMA mode can transmit up to 32 bytes at a time.
 static void
 send_data(spi_config_t *spicfg, const uint8_t *data, int16_t len, int32_t dc)
 {
   esp_err_t err;
   spi_transaction_t tx;
+
+  // spi pre-transfer setting, control lines.
+  gpio_set_level(spicfg->num_cs, 0);
+  gpio_set_level(spicfg->num_dc, dc);
 
   if (spicfg->dma_ch == 0) {
     // NO_DMA mode
@@ -284,26 +283,37 @@ send_data(spi_config_t *spicfg, const uint8_t *data, int16_t len, int32_t dc)
       ESP_LOGI(TAG, "send_data: spi_device_get_trans_result error=%d", err);
     }
   }
+  // spi post-transfer setting, control lines.
+  gpio_set_level(spicfg->num_dc, 0);
+  gpio_set_level(spicfg->num_cs, 1);
 }
+
+// SSD1306 init commands
+DRAM_ATTR static const uint8_t ssd1306_address_init[] = {
+  0x20, 0x00,         // ADDR_MODE, 0x00 = Horizontal Mode
+  0x21, 0x00, 0x7F,   // COLUMN_ADDR, 0x00 = start, 0x7f = end
+  0x22, 0x00, 0x07    // PAGE_ADDR, 0x00 = start, 0x7f = end
+};
 
 // Send buffer to display
 static void
-ssd1306_display(spi_config_t *spicfg)
+ssd1306_send_display(spi_config_t *spicfg)
 {
   uint8_t *buffer;
 
   if (spicfg->dma_ch == 0) {
     // NO DMA
-    buffer = (uint8_t *)malloc(SSD1306_DISPLAY_PIXSEL);
+    buffer = (uint8_t *)malloc(spicfg->tinygrafx.display_pixel);
   } else {
     // Use DMA_CH1 or DMA_CH2
-    buffer = (uint8_t *)heap_caps_malloc(SSD1306_DISPLAY_PIXSEL, MALLOC_CAP_DMA);
+    buffer = (uint8_t *)heap_caps_malloc(spicfg->tinygrafx.display_pixel, MALLOC_CAP_DMA);
   }
 
   if (buffer != NULL) {
-    memset(buffer, 0, SSD1306_DISPLAY_PIXSEL);
-    buffer_read(buffer, SSD1306_DISPLAY_PIXSEL);
-    send_data(spicfg, buffer, SSD1306_DISPLAY_PIXSEL, DC_DATA);
+    memset(buffer, 0x00, spicfg->tinygrafx.display_pixel);
+    buffer_read(spicfg->tinygrafx, buffer, spicfg->tinygrafx.display_pixel);
+    send_data(spicfg, ssd1306_address_init, sizeof(ssd1306_address_init), DC_CMD);
+    send_data(spicfg, buffer, spicfg->tinygrafx.display_pixel, DC_DATA);
   }
 
   if (spicfg->dma_ch == 0) {
@@ -317,9 +327,8 @@ ssd1306_display(spi_config_t *spicfg)
 static mrb_value
 ssd1306_spi_display(mrb_state *mrb, mrb_value self)
 {
-  spi_config_t *spicfg = DATA_PTR(self);
-  ssd1306_display(spicfg);
-
+  spi_config_t *spicfg = (spi_config_t *)DATA_PTR(self);
+  ssd1306_send_display(spicfg);
   return mrb_nil_value();
 }
 
@@ -334,29 +343,26 @@ spi_bus_init(spi_config_t *spicfg)
     .quadwp_io_num = -1,  // WP (Write Protect) signal, or -1 if not used.
     .quadhd_io_num = -1   // HD (HolD) signal, or -1 if not used.
   };
-
   spi_device_interface_config_t devcfg = {
     .clock_speed_hz = spicfg->spi_freq,
     .mode = spicfg->spi_mode,
     .spics_io_num = spicfg->num_cs,
-    .queue_size = 1,
-    .pre_cb = spi_pre_transfer_callback,
-    .post_cb = spi_post_transfer_callback
+    .queue_size = 1
   };
-
   esp_err_t err;
   spi_device_handle_t spi;
-
+  
   // Initialize the SPI bus
   err = spi_bus_initialize(SSD1306SPI_HOST, &buscfg, spicfg->dma_ch);
+  spicfg->require_reset = (err == ESP_ERR_INVALID_STATE) ? false : true;
   if (err != ESP_OK) {
-    ESP_LOGI(TAG, "spi_bus_init: spi_bus_initialize error=%d", err);
+    ESP_LOGI(TAG, "spi_bus_init: spi_bus_initialize status=%d", err);
   }
 
   // Attach the OLED to the SPI bus
   err = spi_bus_add_device(SSD1306SPI_HOST, &devcfg, &spi);
   if (err != ESP_OK) {
-    ESP_LOGI(TAG, "spi_bus_init: spi_bus_add_device error=%d", err);
+    ESP_LOGI(TAG, "spi_bus_init: spi_bus_add_device status=%d", err);
   }
 
   // Save the SPI device handle to SPI Object.
@@ -366,6 +372,7 @@ spi_bus_init(spi_config_t *spicfg)
 static void
 spi_deinit(spi_config_t *spicfg)
 {
+  free(spicfg->tinygrafx.display_buffer);
   free(spicfg->spi);
 }
 
@@ -398,24 +405,57 @@ ssd1306_init(spi_config_t *spicfg)
   gpio_set_direction(spicfg->num_rst, GPIO_MODE_OUTPUT);
   gpio_set_direction(spicfg->num_cs, GPIO_MODE_OUTPUT);
 
-  // Reset the display
-  gpio_set_level(spicfg->num_rst, 1);
-  gpio_set_level(spicfg->num_rst, 0);
-  vTaskDelay(10 / portTICK_PERIOD_MS);
-  gpio_set_level(spicfg->num_rst, 1);
+  // Reset the display if host not in use
+  if (spicfg->require_reset) {
+    gpio_set_level(spicfg->num_rst, 1);
+    gpio_set_level(spicfg->num_rst, 0);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    gpio_set_level(spicfg->num_rst, 1);
+  }
 
   // Send all commands
   send_data(spicfg, ssd1306_init_cmds, sizeof(ssd1306_init_cmds), DC_CMD);
 }
 
+// Configuration the Tiny graphics libraries
+static void
+tinygrafx_init(spi_config_t *spicfg)
+{
+  tinygrafx_t tg = {
+    .display_width = SSD1306_DISPLAY_WIDTH,
+    .display_height = SSD1306_DISPLAY_HEIGHT,
+    .display_pixel = SSD1306_DISPLAY_PIXEL,
+    .font_width = SSD1306_FONT_WIDTH,
+    .font_height = SSD1306_FONT_HEIGHT
+  }; 
+  // set frame buffer
+  uint8_t *buffer;
+  buffer = (uint8_t *)malloc(tg.display_pixel);
+  if (buffer != NULL) {
+    memset(buffer, 0, tg.display_pixel);
+  }
+  tg.display_buffer = buffer; 
+
+  spicfg->tinygrafx = tg;
+}
+
+// free mrb object for GC.
+static void
+meb_ssd1306_free(mrb_state *mrb, void *ptr)
+{
+  spi_config_t *spicfg = ptr;
+  mrb_free(mrb, spicfg->tinygrafx.display_buffer);
+  mrb_free(mrb, spicfg->spi);
+}
+
 // mruby data_type
 static const struct mrb_data_type mrb_spi_config_type = {
-  "spi_config_type", mrb_free
+  "spi_config_type", meb_ssd1306_free
 };
 
 // Initialize the SPI device for SSD1306
 static mrb_value
-spi_init(mrb_state *mrb, mrb_value self)
+ssd1306_spi_init(mrb_state *mrb, mrb_value self)
 {
   spi_config_t *spicfg = (spi_config_t *)DATA_PTR(self);
   if (spicfg) {
@@ -440,7 +480,7 @@ spi_init(mrb_state *mrb, mrb_value self)
   spicfg->dma_ch   = dma_ch;
   DATA_TYPE(self) = &mrb_spi_config_type;
   DATA_PTR(self)  = spicfg;
-
+  
   // Initialize the SPI
   spi_bus_init(spicfg);
 
@@ -448,40 +488,28 @@ spi_init(mrb_state *mrb, mrb_value self)
   ssd1306_init(spicfg);
 
   // Initialize the TINYGRAFX
-  tinygrafx_config_t config = {
-    .display_width = SSD1306_DISPLAY_WIDTH,
-    .display_height = SSD1306_DISPLAY_HEIGHT,
-    .display_pixsel = SSD1306_DISPLAY_PIXSEL,
-    .font_width = SSD1306_FONT_WIDTH,
-    .font_height = SSD1306_FONT_HEIGHT
-  };
-  tinygrafx_init(config);
-
+  tinygrafx_init(spicfg);
+  
   return self;
 }
 
-// Object duplication method
-static mrb_value
-spi_init_copy(mrb_state *mrb, mrb_value copy)
-{
-  mrb_value src;
-
-  mrb_get_args(mrb, "o", &src);
-  if (mrb_obj_equal(mrb, copy, src)) return copy;
-  
-  if (!mrb_obj_is_instance_of(mrb, src, mrb_obj_class(mrb, copy))) {
-    mrb_raise(mrb, E_TYPE_ERROR, "wrong argument class");
-  }
-  
-  if (!DATA_PTR(copy)) {
-    DATA_PTR(copy) = (spi_config_t *)mrb_malloc(mrb, sizeof(spi_config_t));
-    DATA_TYPE(copy) = &mrb_spi_config_type;
-  }
-
-  *(spi_config_t *)DATA_PTR(copy) = *(spi_config_t *)DATA_PTR(src);
-  
-  return copy;
-}
+// // Object duplication method
+// static mrb_value
+// spi_init_copy(mrb_state *mrb, mrb_value copy)
+// {
+//   mrb_value src;
+//   mrb_get_args(mrb, "o", &src);
+//   if (mrb_obj_equal(mrb, copy, src)) return copy;
+//   if (!mrb_obj_is_instance_of(mrb, src, mrb_obj_class(mrb, copy))) {
+//     mrb_raise(mrb, E_TYPE_ERROR, "wrong argument class");
+//   }
+//   if (!DATA_PTR(copy)) {
+//     DATA_PTR(copy) = (spi_config_t *)mrb_malloc(mrb, sizeof(spi_config_t));
+//     DATA_TYPE(copy) = &mrb_spi_config_type;
+//   }
+//   *(spi_config_t *)DATA_PTR(copy) = *(spi_config_t *)DATA_PTR(src);
+//   return copy;
+// }
 
 // View config setting
 static mrb_value
@@ -498,7 +526,6 @@ spi_view_config(mrb_state *mrb, mrb_value self)
   mrb_ary_push(mrb, spi_param, mrb_fixnum_value(spicfg->spi_freq));
   mrb_ary_push(mrb, spi_param, mrb_fixnum_value(spicfg->spi_mode));
   mrb_ary_push(mrb, spi_param, mrb_fixnum_value(spicfg->dma_ch));
-
   return spi_param;
 }
 
@@ -513,26 +540,26 @@ mrb_mruby_esp32_spi_ssd1306_gem_init(mrb_state* mrb)
   struct RClass *ssd1306 = mrb_define_class_under(mrb, oled, "SSD1306SPI", mrb->object_class);
   MRB_SET_INSTANCE_TT(ssd1306, MRB_TT_DATA);
 
-  // ssd1306 spi method
-  mrb_define_method(mrb, ssd1306, "_init", spi_init, MRB_ARGS_NONE());
-  mrb_define_method(mrb, ssd1306, "initialize_copy", spi_init_copy, MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, ssd1306, "config?", spi_view_config, MRB_ARGS_NONE());
+  // Common graphics methods
+  mrb_define_method(mrb, ssd1306, "clear", lcd_clear, MRB_ARGS_NONE());
+  mrb_define_method(mrb, ssd1306, "set_pixel", lcd_set_pixel, MRB_ARGS_REQ(2));
+  mrb_define_method(mrb, ssd1306, "get_pixel", lcd_get_pixel, MRB_ARGS_REQ(2));
+  mrb_define_method(mrb, ssd1306, "line", lcd_draw_line, MRB_ARGS_REQ(4));
+  mrb_define_method(mrb, ssd1306, "vline", lcd_draw_vertical_line, MRB_ARGS_REQ(3));
+  mrb_define_method(mrb, ssd1306, "hline", lcd_draw_horizontal_line, MRB_ARGS_REQ(3));
+  mrb_define_method(mrb, ssd1306, "rect", lcd_draw_rect, MRB_ARGS_REQ(4));
+  mrb_define_method(mrb, ssd1306, "fill_rect", lcd_draw_fill_rect, MRB_ARGS_REQ(4));
+  mrb_define_method(mrb, ssd1306, "circle", lcd_draw_circle, MRB_ARGS_REQ(3));
+  mrb_define_method(mrb, ssd1306, "fill_circle", lcd_draw_fill_circle, MRB_ARGS_REQ(3));
+  mrb_define_method(mrb, ssd1306, "text", lcd_text, MRB_ARGS_REQ(3));
 
-  // manipulate graphics
-  mrb_define_method(mrb, ssd1306, "clear", ssd1306_clear, MRB_ARGS_NONE());
-  mrb_define_method(mrb, ssd1306, "set_pixel", ssd1306_set_pixel, MRB_ARGS_REQ(2));
-  mrb_define_method(mrb, ssd1306, "get_pixel", ssd1306_get_pixel, MRB_ARGS_REQ(2));
-  mrb_define_method(mrb, ssd1306, "line", ssd1306_draw_line, MRB_ARGS_REQ(4));
-  mrb_define_method(mrb, ssd1306, "vline", ssd1306_draw_vertical_line, MRB_ARGS_REQ(3));
-  mrb_define_method(mrb, ssd1306, "hline", ssd1306_draw_horizontal_line, MRB_ARGS_REQ(3));
-  mrb_define_method(mrb, ssd1306, "rect", ssd1306_draw_rect, MRB_ARGS_REQ(4));
-  mrb_define_method(mrb, ssd1306, "fill_rect", ssd1306_draw_fill_rect, MRB_ARGS_REQ(4));
-  mrb_define_method(mrb, ssd1306, "circle", ssd1306_draw_circle, MRB_ARGS_REQ(3));
-  mrb_define_method(mrb, ssd1306, "fill_circle", ssd1306_draw_fill_circle, MRB_ARGS_REQ(3));
-
-  // Display a character string
+  // Send frame buffer to display
   mrb_define_method(mrb, ssd1306, "display", ssd1306_spi_display, MRB_ARGS_NONE());
-  mrb_define_method(mrb, ssd1306, "text", ssd1306_text, MRB_ARGS_REQ(3));
+
+  // ssd1306 spi method
+  mrb_define_method(mrb, ssd1306, "_init", ssd1306_spi_init, MRB_ARGS_NONE());
+  // mrb_define_method(mrb, ssd1306, "initialize_copy", spi_init_copy, MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, ssd1306, "config?", spi_view_config, MRB_ARGS_NONE());
 
   struct RClass *constants = mrb_define_module_under(mrb, ssd1306, "Constants");
   mrb_define_const(mrb, constants, "CS",        mrb_fixnum_value(SSD1306SPI_PIN_NUM_CS));
